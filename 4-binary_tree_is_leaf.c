6f42f5c8f34ce4b7bcc784620ffeb2bf7f0cf869
Modibo Keita <modibokeita9390@gmail.com>
2024-03-01 07:41:23 -0800

    4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_is_leaf - checks if a node is a leaf
  * @node: node to check
- * Return: 1 if node 
+ * Return: 1 if node
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
