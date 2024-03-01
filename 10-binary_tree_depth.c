5f1417ca07fc2777c7be16ae8a82d8941c7a5abc
Modibo Keita <modibokeita9390@gmail.com>
2024-03-01 07:54:20 -0800

    10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: node to calculate the depth of
+ * Return: depth of the node
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t dp = 0;
+
+	if (!tree)
+		return (0);
+
+	while (tree->parent)
+	{
+		dp++;
+		tree = tree->parent;
+	}
+
+	return (dp);
+}
